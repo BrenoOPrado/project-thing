dffd8c806dcb16545c88b79efbbb294256f4f664
BrenoOPrado <breno.o.prado@hotmail.com>
2023-04-04 18:29:14 -0300

    Funções exists_word e search_by_word desenvolvidas :turtle:

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,39 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    allData = instance.seeData()
+    result = []
+    for data in allData:
+        res = {
+            "palavra": word,
+            "arquivo": data['nome_do_arquivo'],
+            "ocorrencias": []
+        }
+        for index, line in enumerate(data['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                res['ocorrencias'].append({
+                    'linha': index + 1
+                })
+        if len(res['ocorrencias']) > 0:
+            result.append(res)
+    return result
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    allData = instance.seeData()
+    result = []
+    for data in allData:
+        res = {
+            "palavra": word,
+            "arquivo": data['nome_do_arquivo'],
+            "ocorrencias": []
+        }
+        for index, line in enumerate(data['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                res['ocorrencias'].append({
+                    'linha': index + 1,
+                    'conteudo': line
+                })
+        if len(res['ocorrencias']) > 0:
+            result.append(res)
+    return result
