974501c2fca0a04af5f2106945f2d310ad8b00c6
BrenoOPrado <breno.o.prado@hotmail.com>
2023-04-04 15:21:41 -0300

    Função txt_importer completa :turtle:

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if '.txt' in path_file:
+        try:
+            with open(path_file, 'r') as file:
+                data = file.read()
+                informations = data.split('\n')
+                return informations
+        except FileNotFoundError:
+            sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    else:
+        sys.stderr.write('Formato inválido')
